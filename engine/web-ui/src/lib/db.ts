49d419fea4d6b30e679ebcdc48d07a9e48156181
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2026-04-11 17:41:22 +0700

    fix: remove duplicate

--- engine/web-ui/src/lib/db.ts
+++ engine/web-ui/src/lib/db.ts
@@ -63,16 +63,6 @@ export type {
   SaveCausalArtifactsResult,
 } from "./db-modules/causal-artifacts";
 
-export { getCausalArtifactsForItem, saveCausalArtifacts } from "./db-modules/causal-artifacts";
-
-export type {
-  CausalArtifactsPayload,
-  ExtractionPayloadRecord,
-  FollowUpExportRecord,
-  SaveCausalArtifactsInput,
-  SaveCausalArtifactsResult,
-} from "./db-modules/causal-artifacts";
-
 export { migrateLegacyData } from "./db-modules/migration";
 
 export { default } from "./db-modules/connection";
